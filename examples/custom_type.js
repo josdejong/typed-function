9c1ecc565b7fe494b05709b2e6370dbc63385856
jos <wjosdejong@gmail.com>
2014-10-23 22:06:29 +0200

    Added another example

---
+++ examples/custom_type.js
@@ -0,0 +1,43 @@
+var compose = require('../index');
+
+// create a prototype
+function Person(params) {
+  this.name = params.name;
+  this.age = params.age;
+}
+
+// register a test for this new type
+compose.tests['Person'] = function (x) {
+  return x instanceof Person;
+};
+
+// compose a function
+var stringify = compose({
+  'Person': function (person) {
+    return JSON.stringify(person);
+  },
+  'Person,number': function (person, indentation) {
+    return JSON.stringify(person, null, indentation);
+  }
+});
+
+// use the function
+var person = new Person({name: 'John', age: 28});
+
+console.log(stringify(person));
+// outputs: '{"name":"John","age":28}'
+
+console.log(stringify(person, 2));
+// outputs with indentation:
+//   '{
+//     "name": "John",
+//     "age": 28
+//   }'
+
+// calling the function with a non-supported type signature will throw an error
+try {
+  stringify('ooops');
+}
+catch (err) {
+  console.log(err.toString()); // outputs: 'TypeError: Wrong function signature'
+}
