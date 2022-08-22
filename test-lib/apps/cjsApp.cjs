4ad68f2e7e4a42dd037e30a62324a162e9c7db1f
Jos de Jong <wjosdejong@gmail.com>
2022-08-22 15:33:22 +0200

    Create an UMD build too

--- test-lib/apps/cjsApp.cjs
+++ test-lib/apps/cjsApp.cjs
@@ -1,17 +1,13 @@
-async function run () {
-  const typed = (await import('../../lib/esm/typed-function.js')).default
+const typed = require('../../lib/umd/typed-function.js')
 
-  // create a typed function
-  const fn1 = typed({
-    'number, string': function (a, b) {
-      return 'a is a number, b is a string'
-    }
-  })
+// create a typed function
+const fn1 = typed({
+  'number, string': function (a, b) {
+    return 'a is a number, b is a string'
+  }
+})
 
-  // use the function
-  // outputs 'a is a number, b is a string'
-  const result = fn1(2, 'foo')
-  console.log(result)
-}
-
-run().catch(console.error)
+// use the function
+// outputs 'a is a number, b is a string'
+const result = fn1(2, 'foo')
+console.log(result)
